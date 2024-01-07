90b6c3a290d342653e71de6c5a6b4327e23d61c6
zanadoman <domanseven@gmail.com>
2024-01-07 22:16:51 +0100

    .

--- main.cpp
+++ main.cpp
@@ -4,17 +4,10 @@ using namespace NeoTypes;
 
 int main(int argc, char* *argv)
 {
-    array<uint8> arr1;
+    string* str;
 
-    arr1 += 1;
-    arr1 += 2;
-
-    arr1.Insert(0, 3);
-
-    for (uint64 i = 0; i < arr1.Length; i++)
-    {
-        printf("%d\n", arr1[i]);
-    }
+    str = new string("Hello World");
+    printf("%s\n", (*str)());
 
     return 0;
 }
