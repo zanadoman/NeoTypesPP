94c444286e48553896d7364b44337e383525ef74
zanadoman <domanseven@gmail.com>
2024-01-07 02:15:32 +0100

    .

--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@ using namespace NeoTypes;
 
 sint32 main(sint32 argc, char* *argv)
 {
-    printf("%lld\n", strLength((char*)"bruh"));
+    array<uint8> arr(0);
+
+    for (uint8 i = 0; i < 9; i++)
+    {
+        arr.Insert(i, i);
+    }
+
+    arr.Reverse();
+
+    for (uint8 i = 0; i < 10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
