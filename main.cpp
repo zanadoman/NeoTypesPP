dfd54dc2f7b1178e526da451d2ddabb610e5ab0b
zanadoman <domanseven@gmail.com>
2024-01-07 19:14:08 +0100

    .

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-#include "NeoTypes.hpp"
+#include "Source/NeoTypes.hpp"
 
 using namespace NeoTypes;
 
 int main(int argc, char* *argv)
 {
     string str = "hello";
+
+    printf("%s\n", str());
 
     return 0;
 }
