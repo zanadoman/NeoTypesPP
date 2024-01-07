c76122034016bce73a5a6717f67070d817c5de2e
zanadoman <domanseven@gmail.com>
2024-01-07 22:27:09 +0100

    .

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@ using namespace NeoTypes;
 
 int main(int argc, char* *argv)
 {
-    string* str;
+    string str;
+    array<string*> arr;
+    
+    str = "car building bear game hello city";
+    arr = str.Split(' ');
 
-    str = new string("Hello World");
-    printf("%s\n", (*str)());
+    for (uint8 i = 0; i < arr.Length; i++)
+    {
+        printf("%s\n", (*arr[i])());
+    }
 
     return 0;
 }
