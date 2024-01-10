da6765a11d2049534fb7f616b955492eab657b3c
zanadoman <domanseven@gmail.com>
2024-01-10 15:36:47 +0100

    .

--- Source/NeoTypesPP.hpp
+++ Source/NeoTypesPP.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include <memory>
 #include <initializer_list>
-#include <math.h>
+#include <cmath>
 
 namespace neo
 {
