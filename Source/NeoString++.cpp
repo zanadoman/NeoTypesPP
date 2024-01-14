0073f82bbeae22f154fa1e05a460c4925908d75a
zanadoman <domanseven@gmail.com>
2024-01-14 10:25:06 +0100

    .

--- Source/NeoString++.cpp
+++ Source/NeoString++.cpp
@@ -688,6 +688,7 @@ namespace neo
 
         if (Literals.size() != 0)
         {
+            lengthPrev = this->length;
             for (uint64 i = 0; i < Literals.size(); i++)
             {
                 if (Literals.begin()[i] == NULL)
@@ -700,11 +701,7 @@ namespace neo
                     printf("string.Insert(): Literals[%lld] must not be Self\nParams: Literals(length): %ld\n", i, Literals.size());
                     exit(1);
                 }
-            }
 
-            lengthPrev = this->length;
-            for (uint64 i = 0; i < Literals.size(); i++)
-            {
                 this->length += strLength(Literals.begin()[i]) - 1;
             }
 
@@ -746,6 +743,7 @@ namespace neo
 
         if (Strings.size() != 0)
         {
+            lengthPrev = this->length;
             for (uint64 i = 0; i < Strings.size(); i++)
             {
                 if (Strings.begin()[i] == NULL)
@@ -758,11 +756,7 @@ namespace neo
                     printf("string.Insert(): Strings[%lld] must not be Self\nParams: Strings(length): %ld\n", i, Strings.size());
                     exit(1);
                 }
-            }
 
-            lengthPrev = this->length;
-            for (uint64 i = 0; i < Strings.size(); i++)
-            {
                 this->length += Strings.begin()[i]->length - 1;
             }
 
