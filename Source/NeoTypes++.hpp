14507410dc9d633944c4723c1cfbfb85354d9284
zanadoman <domanseven@gmail.com>
2024-01-14 17:44:56 +0100

    .

--- Source/NeoTypes++.hpp
+++ Source/NeoTypes++.hpp
@@ -822,6 +822,54 @@ namespace neo
 
     template <typename type> template <typename typeTMP> typename std::enable_if<std::is_same<typeTMP, string>::value, array<typeTMP>*>::type array<type>::ReadFile(const char* Path)
     {
+        FILE* file;
+        char tmp;
+
+        if (Path == NULL)
+        {
+            printf("array.ReadFile(): Path must not be NULL\nParams: Path: %p\n", Path);
+            exit(1);
+        }
+
+        for (uint64 i = 0; i < this->length; i++)
+        {
+            this->allocator.destroy(&this->elements[i]);
+        }
+        if ((this->elements = (type*)realloc(this->elements, sizeof(type) * (this->length = 1))) == NULL)
+        {
+            printf("array.ReadFile(): Memory allocation failed\nParams: Path: %s\n", Path);
+            exit(1);
+        }
+        this->allocator.construct(&this->elements[0]);
+
+        if ((file = fopen(Path, "r")) != NULL)
+        {
+            while (true)
+            {
+                tmp = fgetc(file);
+
+                if (feof(file))
+                {
+                    if (this->elements[this->length - 1].Length() == 1)
+                    {
+                        this->Remove(this->length - 1, 1);
+                    }
+                    break;
+                }
+
+                if (tmp != '\n')
+                {
+                    this->elements[this->length - 1] += {tmp};
+                }
+                else if (1 < this->elements[this->length - 1].Length())
+                {
+                    this->Insert(this->length, 1);
+                }
+            }
+        }
+
+        fclose(file);
+
         return this;
     }
 
