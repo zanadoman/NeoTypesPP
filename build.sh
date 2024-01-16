0bb2c667a47ae0815507bdc8f4d9943ff0b0ec29
zanadoman <domanseven@gmail.com>
2024-01-16 11:16:10 +0100

    .

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cp Source/*.hpp 'Linux (x86_64)'
-gcc -c Source/*.cpp -m64 -std=gnu++23
+g++ -c Source/*.cpp -m64 -std=gnu++23
 ar rcs 'Linux (x86_64)/libNeoTypes++.a' *.o
 rm *.o
 
 cp Source/*.hpp 'Windows (x86_64)'
-x86_64-w64-mingw32-gcc -c Source/*.cpp -m64 -std=gnu++23
+x86_64-w64-mingw32-g++ -c Source/*.cpp -m64 -std=gnu++23
 ar rcs 'Windows (x86_64)/libNeoTypes++.a' *.o
 rm *.o
