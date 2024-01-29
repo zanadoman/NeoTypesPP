401194dc39b713dcae14b7937cffb78ef3b4ffb8
zanadoman <domanseven@gmail.com>
2024-01-29 11:31:28 +0100

    .

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -o test.out $(find . -name '*.cpp') -m64 -std=gnu++23 -lm && time ./test.out
+g++ -Wall -Wextra -O3 -o test.out $(find . -name '*.cpp') -m64 -std=gnu++23 -lm && time ./test.out
