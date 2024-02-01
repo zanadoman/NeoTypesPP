a5599340b41cfc87a6e38f9a269f4e1683c9837b
zanadoman <domanseven@gmail.com>
2024-02-01 10:15:59 +0100

    .

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 g++ -m64 -std=gnu++23 -O3 -c Source/*.cpp
-ar rcs "Linux (x86_64)/libNeoTypes++.a" *.o
+ar rcs "Linux (x86_64)/libNeoTypes++.a" ./*.o
 cp Source/*.hpp "Linux (x86_64)"
-rm *.o
+rm ./*.o
 
 x86_64-w64-mingw32-g++ -m64 -std=gnu++23 -O3 -c Source/*.cpp
-ar rcs "Windows (x86_64)/libNeoTypes++.a" *.o
+ar rcs "Windows (x86_64)/libNeoTypes++.a" ./*.o
 cp Source/*.hpp "Windows (x86_64)"
-rm *.o
+rm ./*.o
