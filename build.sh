c132e7a5258a422430ee197e9d67029013da72a1
zanadoman <domanseven@gmail.com>
2024-02-28 12:00:14 +0100

    .

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-g++ -m64 -std=gnu++23 -O3 -c src/*.cpp
-ar rcs "build/lib/linux/libNeoTypes++.a" ./*.o
+g++ -m64 -std=gnu++23 -O3 -c -fPIC src/*.cpp
+g++ -shared -o "build/lib/linux/libNeoTypes++.so" ./*.o
 rm ./*.o
 
-x86_64-w64-mingw32-g++ -m64 -std=gnu++23 -O3 -c src/*.cpp
-ar rcs "build/lib/windows/libNeoTypes++.a" ./*.o
+x86_64-w64-mingw32-g++ -m64 -std=gnu++23 -O3 -c -fPIC src/*.cpp
+x86_64-w64-mingw32-g++ -shared -o "build/lib/windows/libNeoTypes++.dll" ./*.o
 rm ./*.o
 
 cp src/*.hpp build/inc
